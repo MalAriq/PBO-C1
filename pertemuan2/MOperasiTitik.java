60897feaedca0d7f9cbb23a69bf8bb8b1339ad78
Kamal <100132773+MalAriq@users.noreply.github.com>
2023-03-01 19:55:44 +0700

    Add files via upload

---
+++ pertemuan2/MOperasiTitik.java
@@ -0,0 +1,14 @@
+public class MOperasiTitik{
+
+    public static void main(String[] args){
+        Titik t = new Titik(4.0, 4.0);
+        OperasiTitik o = new OperasiTitik();
+
+        System.out.println("Titik : ("+t.getAbsis()+", "+t.getOrdinat()+")");
+        o.refleksiSumbuX(t);//hasil
+        System.out.println("Titik : ("+t.getAbsis()+", "+t.getOrdinat()+")");
+        o.refleksiSumbuY(t);
+        System.out.println("Titik : ("+t.getAbsis()+", "+t.getOrdinat()+")");  
+
+    }
+}
